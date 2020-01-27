ccd20cdecca0f08f4ee5380e646ac817f17907d8
zongxinc <45567357+zongxinc@users.noreply.github.com>
2020-01-27 10:51:49 -0500

    Update arabic_roman.py

--- arabic_roman.py
+++ arabic_roman.py
@@ -1,4 +1,4 @@
-def arabic_to_roman(num):
+def arabic_roman(num):
     table = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], 
     [50, 'L'], [40, 'XL'],[10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
     res = ''
@@ -8,4 +8,4 @@ def arabic_to_roman(num):
     return res
 
 for i in 1,4,9,16,25,49,81,1963,2015:
-    print i, arabic_to_roman(i)
+    print i, arabic_roman(i)
