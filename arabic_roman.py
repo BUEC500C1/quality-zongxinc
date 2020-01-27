f6c8a870ff6b149248dde2073a1bbda7685b2059
zongxinc <45567357+zongxinc@users.noreply.github.com>
2020-01-27 17:42:46 -0500

    Update arabic_roman.py

--- arabic_roman.py
+++ arabic_roman.py
@@ -2,8 +2,10 @@ def arabic_roman(num):
     table = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], 
     [50, 'L'], [40, 'XL'],[10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
     res = ''
-    if num > 9999:
+    if num > 3999:
         return 'too big'
+    if num < 0:
+        return 'no negative'
     for unit, roman in table:
         res += roman * (num//unit)
         num %= unit
