79cae347b07527694823203c74fdd1e56abcde3e
zongxinc <45567357+zongxinc@users.noreply.github.com>
2020-01-27 17:36:57 -0500

    Update test_arabic_roman.py

--- test_arabic_roman.py
+++ test_arabic_roman.py
@@ -2,6 +2,6 @@ from arabic_roman import arabic_roman
 
 def test():
   assert arabic_roman(1) == 'I'
-  assert arabic_roman(1000) == 'too big'
+  assert arabic_roman(10000) == 'too big'
   assert arabic_roman(0) == ''
   
